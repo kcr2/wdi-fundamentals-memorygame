52cf9453c51bd882e345ed8a3916dbc8ed03b1cc
KcRakshyak <47199823+KcRakshyak@users.noreply.github.com>
2019-10-05 17:49:33 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("up and running");
+var card1 = "Queen";
+var card2 = "King";
+var card3 = "Queen";
+var card4 = "King";
+console.log("User flipped " + card1);
+console.log("User flipped " + card2);
